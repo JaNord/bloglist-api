9f54665a6ad9da335d315fa201bc798622400c4a
JaNord <janordma@abo.fi>
2020-10-05 14:07:23 +0300

    exercise 4.8 and 4.9

---
+++ tests/bloglist-api.test.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose")
+const supertest = require("supertest")
+const faker = require("faker")
+
+const app = require("../app")
+const Blog = require("../models/blog")
+
+const api = supertest(app)
+
+const initialBlogs = [
+    {
+        title: faker.name.firstName(),
+        author: faker.random.word(),
+        url: faker.internet.url(),
+        likes: faker.random.number()
+    },
+    {
+        title: faker.name.firstName(),
+        author: faker.random.word(),
+        url: faker.internet.url(),
+        likes: faker.random.number()
+    },
+    {
+        title: faker.name.firstName(),
+        author: faker.random.word(),
+        url: faker.internet.url(),
+        likes: faker.random.number()
+    }
+]
+// test setup
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+})
+afterAll(() => {
+    mongoose.connection.close()
+})
+
+// actual tests
+describe("HTTP GET", () => {
+    test("should return the correct amount of blogs", async() => {
+        const response = await api.get("/api/blogs")
+        
+        expect(response.body).toHaveLength(2)
+    })
+
+    test("items returned should have identifier propery name id", async() => {
+        const response = await api.get("/api/blogs")
+        
+        response.body.forEach(blog => {
+            expect(blog.id).toBeDefined()  
+        })
+    })
+})
