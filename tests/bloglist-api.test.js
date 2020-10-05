c450875dbcd5bb04c0acd7b6494c7d87f1ba80f0
JaNord <janordma@abo.fi>
2020-10-05 14:24:53 +0300

    exercise 4.10

--- tests/bloglist-api.test.js
+++ tests/bloglist-api.test.js
@@ -41,7 +41,7 @@ afterAll(() => {
     mongoose.connection.close()
 })
 
-// actual tests
+//actual tests
 describe("HTTP GET", () => {
     test("should return the correct amount of blogs", async() => {
         const response = await api.get("/api/blogs")
@@ -55,5 +55,17 @@ describe("HTTP GET", () => {
         response.body.forEach(blog => {
             expect(blog.id).toBeDefined()  
         })
+    })
+})
+
+describe("HTTP POST", () => {
+    test("should save a valid blog to the db", async () => {
+        const result = await api
+            .post("/api/blogs")
+            .send(initialBlogs[2])
+        expect(result.body.title).toEqual(initialBlogs[2].title)
+
+        const response = await api.get("/api/blogs")
+        expect(response.body).toHaveLength(3)
     })
 })
