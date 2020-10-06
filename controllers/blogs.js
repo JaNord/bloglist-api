31cf2ff243a1bc16d6070a34ed8c813dbb9c2ba6
JaNord <janordma@abo.fi>
2020-10-06 15:32:34 +0300

    exercise 4.13-4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,16 +1,18 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
 
+// get all blogs
 blogsRouter.get("/", async (request, response) => {  
-    const blogs = await  Blog.find({})
+    const blogs = await Blog.find({})
     
     response.json(blogs)
 })
 
+// create new blog
 blogsRouter.post("/", async (request, response) => {
-    const data = request.body
+    const body = request.body
 
-    if (!data.title || !data.url) {
+    if (!body.title || !body.url) {
         const errorResponse = {
             error: "Properies title or url cannot be undefined"
         }
@@ -18,13 +20,35 @@ blogsRouter.post("/", async (request, response) => {
         return response.status(400).json(errorResponse)
     }
 
-    if (!data.likes) {
-        data.likes = 0
+    if (!body.likes) {
+        body.likes = 0
     }
-    const blog = new Blog(data)
+    const blog = new Blog(body)
 
     const result = await blog.save()
     response.status(201).json(result)
+})
+
+// update blog by :id
+blogsRouter.put("/:id", async (request, response) => {
+    const body = request.body
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    }
+
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+
+    response.json(updatedBlog)
+})
+
+// delete blog by :id
+blogsRouter.delete("/:id", async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+
+    response.status(204).send()
 })
 
 module.exports = blogsRouter
