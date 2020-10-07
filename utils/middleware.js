69ebf3db8138e51adeeb2acd0911b33846158f8e
JaNord <janordma@abo.fi>
2020-10-07 11:42:25 +0300

    exercise 4.19

--- utils/middleware.js
+++ utils/middleware.js
@@ -8,11 +8,18 @@ const errorHandler = (error, request, response, next) => {
     logger.error(error.message)
 
     if (error.name === "CastError") {
-        return response.status(400).send({error: "malformated id"})
+        return response.status(400).send({
+            error: "malformated id"
+        })
     }
 
     else if (error.name === "ValidationError") {
-        return response.status(400).send({error: error.message})
+        return response.status(400).json({error: error.message})
+    }
+    else if (error.name === "JsonWebTokenError") {
+        return response.status(401).json({
+            error: "invalid token" 
+        })
     }
 
     next(error)
